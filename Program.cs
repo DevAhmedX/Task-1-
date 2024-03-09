b0b4192b321c512cc9e3949f39cd715cf3d5256e
DevAhmedX <162572837+DevAhmedX@users.noreply.github.com>
2024-03-09 17:05:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a word: ");
+        string word = Console.ReadLine();
+
+        string reversedWord = new string(word.ToCharArray().Reverse().ToArray());
+        bool isPalindrome = word.Equals(reversedWord, StringComparison.OrdinalIgnoreCase);
+
+        Console.WriteLine($"{word} is {(isPalindrome ? "a palindrome" : "not a palindrome")}");
+    }
+}
